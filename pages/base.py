43838cf7d7bb19c56b42f3e41e615f9ca49f95b9
Moultree <okaykudes@gmail.com>
2023-03-22 14:59:11 +0600

    disable spacebar scrolling

--- pages/base.py
+++ pages/base.py
@@ -4,5 +4,6 @@ from nicegui import ui
 class Base:
     def __init__(self) -> None:
         ui.add_head_html("<link rel='stylesheet' href='/static/style.css' />")
+        ui.add_head_html("<script src='/static/script.js'> </script>")
 
         self.base = ui.column().classes("main")
