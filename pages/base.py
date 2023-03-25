328579e6afea0447686b9299e7cf5c0310d2c7d5
Moultree <okaykudes@gmail.com>
2023-03-25 22:56:21 +0600

    rollback

--- pages/base.py
+++ pages/base.py
@@ -4,6 +4,5 @@ from nicegui import ui
 class Base:
     def __init__(self):
         ui.add_head_html("<link rel='stylesheet' href='/static/style.css' />")
-        ui.add_head_html("<script src='/static/script.js'> </script>")
 
         self.base = ui.column().classes("main")
