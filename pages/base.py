2280528975c4894a50eea325d9ca7edeb58e9685
Nikolay <okaykudes@gmail.com>
2023-04-11 10:58:26 +0500

    add background music looping and typing effect

--- pages/base.py
+++ pages/base.py
@@ -8,7 +8,8 @@ class Base:
     def __init__(self) -> None:
         ui.colors(primary="#8A7A73", secondary="#FFFFFF")
         ui.add_head_html("<link rel='stylesheet' href='/static/style.css' />")
-        ui.audio("/static/escape-plan.mp3", controls=False, autoplay=True)
 
+        audio = ui.audio("/static/escape-plan.mp3", controls=False, autoplay=True)
+        audio._props["loop"] = True
         self.base = ui.column().classes("main")
         self.db = Database()
