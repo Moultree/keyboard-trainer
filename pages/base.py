fc28e71ef24c0e65fae6d448a8846084ba8516af
Nikolay <okaykudes@gmail.com>
2023-04-11 10:59:50 +0500

    pep8 editing

--- pages/base.py
+++ pages/base.py
@@ -9,7 +9,10 @@ class Base:
         ui.colors(primary="#8A7A73", secondary="#FFFFFF")
         ui.add_head_html("<link rel='stylesheet' href='/static/style.css' />")
 
-        audio = ui.audio("/static/escape-plan.mp3", controls=False, autoplay=True)
+        audio = ui.audio(
+            "/static/escape-plan.mp3",
+            controls=False,
+            autoplay=True)
         audio._props["loop"] = True
         self.base = ui.column().classes("main")
         self.db = Database()
